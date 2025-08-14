a9fae47014f803b862a9ad1f52065bc4dfe6df84
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-08-14 16:27:57 -0600

    draft: updates to setup.py

--- projects/hipblaslt/tensilelite/setup.py
+++ projects/hipblaslt/tensilelite/setup.py
@@ -53,28 +53,27 @@ setup(
   license="MIT",
   install_requires=read_requirements_from_txt(),
   python_requires='>=3.5',
-  packages=["Tensile", "rocisa"],
-  package_data={ "Tensile": ["Tensile/cmake/*"] },
-  data_files=[ ("cmake", ["Tensile/cmake/TensileConfig.cmake", "Tensile/cmake/TensileConfigVersion.cmake"]) ],
+  packages=["Tensile"],
   include_package_data=True,
   entry_points={"console_scripts": [
-    # user runs a benchmark
-    "Tensile = Tensile.Tensile:main",
-    "tensileBenchmarkLibraryClient = Tensile.TensileBenchmarkLibraryClient:main",
-    # CMake calls this to create Tensile.lib
-    "TensileCreateLibrary = Tensile.TensileCreateLibrary:TensileCreateLibrary",
+    "TensileCreateLibrary = Tensile.TensileCreateLibrary",
+    # # user runs a benchmark
+    # "Tensile = Tensile.Tensile:main",
+    # "tensileBenchmarkLibraryClient = Tensile.TensileBenchmarkLibraryClient:main",
+    # # CMake calls this to create Tensile.lib
+    # "TensileCreateLibrary = Tensile.TensileCreateLibrary:TensileCreateLibrary",
 
-    "TensileGetPath = Tensile:PrintTensileRoot",
-    # automatic benchmarking for rocblas
-    "tensile_rocblas_sgemm = Tensile.Tensile:TensileROCBLASSGEMM",
-    "tensile_rocblas_dgemm = Tensile.Tensile:TensileROCBLASDGEMM",
-    "tensile_rocblas_cgemm = Tensile.Tensile:TensileROCBLASCGEMM",
-    "tensile_rocblas_zgemm = Tensile.Tensile:TensileROCBLASZGEMM",
-    # automatically find fastest sgemm exhaustive search
-    "tensile_sgemm = Tensile.Tensile:TensileSGEMM5760",
-    # Run tensile benchmark from cluster
-    "TensileBenchmarkCluster = Tensile.TensileBenchmarkCluster:main",
-    # Retune library logic file
-    "TensileRetuneLibrary = Tensile.TensileRetuneLibrary:main"
+    # "TensileGetPath = Tensile:PrintTensileRoot",
+    # # automatic benchmarking for rocblas
+    # "tensile_rocblas_sgemm = Tensile.Tensile:TensileROCBLASSGEMM",
+    # "tensile_rocblas_dgemm = Tensile.Tensile:TensileROCBLASDGEMM",
+    # "tensile_rocblas_cgemm = Tensile.Tensile:TensileROCBLASCGEMM",
+    # "tensile_rocblas_zgemm = Tensile.Tensile:TensileROCBLASZGEMM",
+    # # automatically find fastest sgemm exhaustive search
+    # "tensile_sgemm = Tensile.Tensile:TensileSGEMM5760",
+    # # Run tensile benchmark from cluster
+    # "TensileBenchmarkCluster = Tensile.TensileBenchmarkCluster:main",
+    # # Retune library logic file
+    # "TensileRetuneLibrary = Tensile.TensileRetuneLibrary:main"
     ]},
   )
